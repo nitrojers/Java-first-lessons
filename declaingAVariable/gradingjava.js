36da2e003100c2fcb0eea7a97f2c4717048ef19f
jeremiah <adebayojeremiah923@gmail.com>
2026-03-08 12:29:19 -0700

    THis is a repository for declaring a variable

---
+++ declaingAVariable/gradingjava.js
@@ -0,0 +1,20 @@
+// I will be declaring variables
+// variable 1
+let firstName = " Johnson "// this is a string variable
+let age = 59 // integer variable
+let currentHeight = 5.8;   // decimal Or float variable
+let asUserBeenBanned = false; // this is a boolean variable
+
+
+
+// here i practiced my typeof
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof currentHeight);
+console.log(typeof asUserBeenBanned);
+
+
+
+// here i practiced the template literals
+console.log(`congratulations ${firstName}, and i must say you look younger than your current age ${age} `);
+console.log(`you are ${currentHeight} tall, and you are just ${age} mister ${firstName}`);
